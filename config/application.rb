a0f702e91e9d4e5a482bb0bc03b62351ca7ea4f1
GonzaVega <gonza.vega@icloud.com>
2023-05-04 11:46:40 -0300

    app with authentiication gem incorporated, cors configured

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module MovementsTrackerBack
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch]
       end
     end
     # Configuration for the application, engines, and railties goes here.
